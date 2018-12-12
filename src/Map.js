0450af00275e738f05155795633c34e436d58b37
dhirajprakash <dhiraj.prakash.30aug@gmail.com>
2018-12-12 14:36:17 -0200

    Changes on Map (#5)
    * Changes on Map
    
    * Minor changes
    
    * Minor changes

--- src/Map.js
+++ src/Map.js
@@ -3,10 +3,10 @@ import './App.css';
 import GoogleMapReact from 'google-map-react';
 import FaMapMarker from 'react-icons/lib/fa/map-marker';
 
-const MapComponent = ({ iconColor }) => (
+const MapComponent = (props) => (
     <div>
         {/* {iconColor} */}
-    <FaMapMarker style={{color: iconColor}} size={20}/>
+    <FaMapMarker style={{color: props.iconColor}} onClick={props.clicked} size={20}/>
     </div>
 );
 
@@ -15,19 +15,31 @@ class Map extends Component {
     constructor(props) {
         super(props);
     }
+
+      
+    mapClickHandler = (name) => {
+        console.log(name);
+        
+    }
+
     render() {
 const mapComp = this.props.mapData.map(mp=>{
     let iconColor = '#FF5733';
+    let iconType = 0;
     if(mp.Rubrica != null){
         if(mp.Rubrica.includes('urto')){
             iconColor='#36A2EB';
+            iconType=1;
         }else if(mp.Rubrica.includes('oubo')){
             iconColor='#FF6384';
+            iconType=2;
          }else{
             iconColor='#FFCE56';
+            iconType=3;
          }
         }else{
             iconColor='#FF5733';
+            iconType=4;
         }
    return (
     
@@ -35,7 +47,16 @@ const mapComp = this.props.mapData.map(mp=>{
            lat={mp.lat}
            lng={mp.lng}
            bo={mp.name}
+           autoria={mp.Autoria}
+           rubrica={mp.Rubrica}
+           periodo={mp.OCCURENCIA_TIME}
+           dia={mp.OCCURENCIA_DAY}
+           tipolocal={mp.TipoDeLocal}
+           especie={mp.Especie}
+           flagrante={mp.Flagrante}
+           clicked={this.mapClickHandler.bind(this,mp.name)}
            iconColor={iconColor}
+           iconType={iconType}
            
 
        />
