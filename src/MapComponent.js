7eb3a2d214fd1d5b00db2049f267d34c5068954f
Ravish <mail2ravysh@gmail.com>
2018-12-13 03:12:41 +0530

    map fixed

--- src/MapComponent.js
+++ src/MapComponent.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import FaMapMarker from 'react-icons/lib/fa/map-marker';
 import FaCab from 'react-icons/lib/fa/cab';
-import {Popover, PopoverBody} from 'reactstrap';
 
 class MapComponent extends Component {
 
@@ -25,66 +24,63 @@ class MapComponent extends Component {
 
     render() {
 
-
         return (
             <div>
                 <div style={{display: this.props.vehiclePresent ? '' : 'none'}}>
-                    <FaCab onClick={this.toggleCarInfoPopper} id={'car_'+this.props.key} style={{color: this.props.iconColor, display: this.props.vehiclePresent ? '' : 'none'}} size={20}/>
-                    <Popover isOpen={this.state.carInfoPopperOpen} className="bg-dark text-white" placement="top" toggle={this.toggleCarInfoPopper} target={'car_'+this.props.key}>
-                        <PopoverBody className="text-white">
-                            <div>
-                                <table>
-                                    <tbody>
-                                    <tr>
-                                        <td>
-                                            Boletim No.
-                                        </td>
-                                        <td className="text-warning">
-                                            {this.props.reportId}
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>
-                                            Ocorrência
-                                        </td>
-                                        <td className="text-warning">
-                                            {this.props.occurencia}
-                                        </td>
-                                    </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </PopoverBody>
-                    </Popover>
+                    <FaCab onClick={this.toggleCarInfoPopper} style={{color: this.props.iconColor, display: this.props.vehiclePresent ? '' : 'none'}} size={20}/>
+                    <div style={{display: this.state.carInfoPopperOpen ? '' : 'none'}}>
+                        <div className="bg-dark text-white" style={{maxHeight: 120, overflowY: 'auto', maxWidth: 150,minWidth:150, overflowX: 'auto'}}>
+                        <table>
+                            <tbody>
+                            <tr>
+                                <td>
+                                    Boletim No.
+                                </td>
+                                <td className="text-warning">
+                                    {this.props.reportId}
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    Ocorrência
+                                </td>
+                                <td className="text-warning">
+                                    {this.props.occurencia}
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                        </div>
+                    </div>
+
                 </div>
                 <div style={{display: this.props.vehiclePresent ? 'none' : ''}}>
-                    <FaMapMarker onClick={this.toggleInfoPopper} id={'marker_'+this.props.key} style={{color: this.props.iconColor, display: this.props.vehiclePresent ? 'none' : ''}} size={20}/>
-                    <Popover isOpen={this.state.infoPopperOpen} className="bg-dark" placement="top" toggle={this.toggleInfoPopper} target={'marker_'+this.props.key}>
-                        <PopoverBody className="text-white">
-                            <div>
-                                <table>
-                                    <tbody>
-                                        <tr>
-                                            <td>
-                                                Boletim No.
-                                            </td>
-                                            <td className="text-warning">
-                                                {this.props.reportId}
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td>
-                                                Ocorrência
-                                            </td>
-                                            <td className="text-warning">
-                                                {this.props.occurencia}
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </PopoverBody>
-                    </Popover>
+                    <FaMapMarker onClick={this.toggleInfoPopper} style={{color: this.props.iconColor, display: this.props.vehiclePresent ? 'none' : ''}} size={20}/>
+                    <div style={{display: this.state.infoPopperOpen ? '' : 'none'}}>
+                        <div className="bg-dark text-white" style={{maxHeight: 120, overflowY: 'auto', maxWidth: 150,minWidth:150, overflowX: 'auto'}}>
+                        <table>
+                            <tbody>
+                            <tr>
+                                <td>
+                                    Boletim No.
+                                </td>
+                                <td className="text-warning">
+                                    {this.props.reportId}
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    Ocorrência
+                                </td>
+                                <td className="text-warning">
+                                    {this.props.occurencia}
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         );
